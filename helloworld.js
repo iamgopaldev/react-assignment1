6827d7aa64201df557adce2605d053b9a57d5d2c
iamgopal <iamgopal.dev@gmail.com>
2024-12-20 23:04:54 +0530

    Added all the react project files and React assignment doc file

---
+++ helloworld.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// var hOneReact = React.createElement("h1", {}, "Hello World! Using React");
+
+const NestedElement = function () {
+  return <>
+    <h2>Using JSX</h2>
+    <p>React is a JavaScript library for building user interfaces</p>
+  </>
+}
+
+
+var htmlRoot = document.getElementById("root");
+var rootReact = ReactDOM.createRoot(htmlRoot);
+rootReact.render(<NestedElement />);
